3a7b7bdcef7c9bb7a4c3e84f233b2fd14f4a470e
Shazia252 <161409243+Shazia252@users.noreply.github.com>
2024-03-26 10:18:39 -0700

    Adding a number guessing game

---
+++ indx.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("welcome to codewithshazia - CLI number Guessing Game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "Enter your guess number(Number from Limit from 1 to 5):",
+    },
+]);
+if (answer.userGussedNumber === randomNumber){
+    console.log("Congratulation ! You Guess a Correct number.");
+}
+else{
+    console.log("Sorry , you guess a wrong number");
+}
